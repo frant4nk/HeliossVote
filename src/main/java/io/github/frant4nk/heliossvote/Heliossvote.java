1ae4655133c15c700c2d52545d4434855d26fc53
frant4nk <72709186+frant4nk@users.noreply.github.com>
2020-10-18 20:22:48 +0200

    Added config file and custom commands

--- src/main/java/io/github/frant4nk/heliossvote/Heliossvote.java
+++ src/main/java/io/github/frant4nk/heliossvote/Heliossvote.java
@@ -1,18 +1,69 @@
 package io.github.frant4nk.heliossvote;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public final class Heliossvote extends JavaPlugin {
+
+    private File customConfigFile;
+    private FileConfiguration customConfig;
 
     @Override
     public void onEnable()
     {
-        getServer().getPluginManager().registerEvents(new VoteEventListener(), this);
+        createCustomConfig();
+        getServer().getPluginManager().registerEvents(new VoteEventListener(this), this);
     }
 
     @Override
     public void onDisable()
     {
 
+    }
+
+    public FileConfiguration getCustomConfig()
+    {
+        return this.customConfig;
+    }
+
+    private void createCustomConfig()
+    {
+
+        customConfigFile = new File(getDataFolder(), "/voteConfigFile.yml");
+        if(!customConfigFile.exists())
+        {
+            customConfigFile.getParentFile().mkdirs();
+            copy(getResource("voteConfigFile.yml"), customConfigFile);
+        }
+        customConfig = new YamlConfiguration();
+        try
+        {
+            customConfig.load(customConfigFile);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    static void copy(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/io/github/frant4nk/heliossvote/VoteEventListener.java
+++ src/main/java/io/github/frant4nk/heliossvote/VoteEventListener.java
@@ -8,8 +8,17 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import com.vexsoftware.votifier.model.VotifierEvent;
 
+import java.util.List;
+
 public class VoteEventListener implements Listener
 {
+    Heliossvote plugin;
+
+    public VoteEventListener(Heliossvote instance)
+    {
+        plugin = instance;
+    }
+
     @EventHandler(priority = EventPriority.NORMAL)
     public void onVotifierEvent(@org.jetbrains.annotations.NotNull VotifierEvent event)
     {
@@ -20,9 +29,23 @@ public class VoteEventListener implements Listener
 
     private void voteTrigger(@org.jetbrains.annotations.NotNull Vote vote)
     {
-        PteroUserAPI api = new PteroUserAPI("https://panel.helioss.co/", "tjUJDkfA0pSZ4U6F1gworR89Wn62aIprBWpi2bLi630MKWkG");
+        List<String> ids = plugin.getCustomConfig().getStringList("ids");
+        String command = plugin.getCustomConfig().getString("command");
+        String replacedCommandGive = command.replace("%player", vote.getUsername());
 
-        UserServer server = api.getServersController().getServer("a9013ecb-244f-4933-8197-99251b79728e");
-        server.sendCommand("say " + vote.getUsername() + " voted!!");
+        String msgCommand = plugin.getCustomConfig().getString("msgcommand");
+        String replacedMsgCommand = msgCommand.replace("%player", vote.getUsername());
+
+        for(String id : ids)
+        {
+            PteroUserAPI api = new PteroUserAPI("https://panel.helioss.co", "tjUJDkfA0pSZ4U6F1gworR89Wn62aIprBWpi2bLi630MKWkG");
+
+            UserServer server = api.getServersController().getServer(id);
+            server.sendCommand(replacedMsgCommand);
+            server.sendCommand(replacedCommandGive);
+        }
+
+        //UserServer server = api.getServersController().getServer("a9013ecb-244f-4933-8197-99251b79728e");
+        //server.sendCommand("say " + vote.getUsername() + " voted!!");
     }
 }
