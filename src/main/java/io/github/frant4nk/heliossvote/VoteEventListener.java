88cedc3c25f018c4498cb266af15bf165cf3ced6
frant4nk <72709186+frant4nk@users.noreply.github.com>
2020-10-18 04:16:46 +0200

    ptero need to be included in jar

--- src/main/java/io/github/frant4nk/heliossvote/VoteEventListener.java
+++ src/main/java/io/github/frant4nk/heliossvote/VoteEventListener.java
@@ -1,17 +1,29 @@
 package io.github.frant4nk.heliossvote;
 
+import com.stanjg.ptero4j.PteroUserAPI;
+import com.stanjg.ptero4j.entities.panel.admin.Server;
+import com.stanjg.ptero4j.entities.panel.user.UserServer;
 import com.vexsoftware.votifier.model.Vote;
-import com.vexsoftware.votifier.model.VotifierEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import com.vexsoftware.votifier.model.VotifierEvent;
 
 public class VoteEventListener implements Listener
 {
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onVotifierEvent(VotifierEvent event)
+    public void onVotifierEvent(@org.jetbrains.annotations.NotNull VotifierEvent event)
     {
         Vote vote = event.getVote();
-        System.out.println("Received: " + vote);
+        System.out.println("Getting vote from: " + vote.getUsername());
+        voteTrigger(vote);
+    }
+
+    private void voteTrigger(@org.jetbrains.annotations.NotNull Vote vote)
+    {
+        PteroUserAPI api = new PteroUserAPI("https://panel.helioss.co/", "tjUJDkfA0pSZ4U6F1gworR89Wn62aIprBWpi2bLi630MKWkG");
+
+        UserServer server = api.getServersController().getServer("a9013ecb-244f-4933-8197-99251b79728e");
+        server.sendCommand("say " + vote.getUsername() + " voted!!");
     }
 }
